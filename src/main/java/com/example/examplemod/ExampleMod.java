a15c48d5911ece712837d9e2af91294cf74f84b7
squid233 <513508220@qq.com>
2020-03-08 16:10:40 +0800

    Main Class and More

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,8 +1,10 @@
 package com.example.examplemod;
 
+import com.example.examplemod.proxy.CommonProxy;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
@@ -14,12 +16,23 @@ public class ExampleMod
     public static final String NAME = "Example Mod";
     public static final String VERSION = "1.0";
 
+    @Mod.Instance(ExampleMod.MODID)
+    private static ExampleMod instance;
+
+    @SidedProxy
+            (clientSide = "com.example.examplemod.ClientProxy",
+             serverSide = "com.example.examplemod.CommonProxy"
+            )
+    private static CommonProxy proxy;
+
     private static Logger logger;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
         logger = event.getModLog();
+        logger.info("preInit");
+        proxy.preInit(event);
     }
 
     @EventHandler
@@ -27,5 +40,6 @@ public class ExampleMod
     {
         // some example code
         logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        proxy.init(event);
     }
 }
---
+++ src/main/java/com/example/examplemod/proxy/CommonProxy.java
@@ -0,0 +1,15 @@
+package com.example.examplemod.proxy;
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public class CommonProxy {
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+}
